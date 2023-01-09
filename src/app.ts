257291469b6c68fe36c4e025ff832592a9372c8e
douglasB834 <douglas_borges09@hotmail.com>
2023-01-09 18:01:04 -0300

    chore: add project structure

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import "express-async-errors";
+import "reflect-metadata";
+import express from "express"
+
+export const app = express();
+app.use(express.json());
+
+//Routes 
+// app.use("", )
+
+
+
+
+
+
