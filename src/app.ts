a1a71f793fffc63c982dee19b8a82a3e0e292ee9
Vinicius <v.nascimento23@yahoo.com.br>
2023-01-09 21:23:54 -0300

    feature: add custom error class and error handling function

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
-import "express-async-errors";
 import "reflect-metadata";
+import "express-async-errors";
 import express from "express"
+import errorHandler from "./errors/errorHandler";
 
 export const app = express();
 app.use(express.json());
 
 //Routes 
-// app.use("", );
+
+// errorHandler DEVE SER SEMPRE O ÚLTIMO DOS app.use()
+app.use(errorHandler);
