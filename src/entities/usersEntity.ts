cc2a85347c77025a5a872d00f7ac29059d421eba
Igor <silveira.igor@outlook.com>
2023-01-10 14:36:39 -0300

    feat: add users entity

---
+++ src/entities/usersEntity.ts
@@ -0,0 +1,37 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity("users")
+export class Users {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({ length: 50 })
+  name: string;
+
+  @Column({ length: 50, unique: true })
+  email: string;
+
+  @Column({ length: 50 })
+  password: string;
+
+  @Column({ length: 15 })
+  phone: string;
+
+  @Column({ length: 200, nullable: true })
+  avatar: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
