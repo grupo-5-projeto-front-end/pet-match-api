b8b856dd6948a7c35ba816fae4ada874d3977ab0
Vinicius <v.nascimento23@yahoo.com.br>
2023-01-12 19:52:20 -0300

    refactor: changed build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
+// build.sh
 #!/usr/bin/env bash
-set -o errexit 
+# exit on error
+set -o errexit
+
 yarn
-yarn build 
-yarn typeorm migration:run -d dist/src/data-source
+yarn build
+yarn typeorm migration:run -d dist/data-source
