37781c418a3a21620042fafc8a5a9cc15470ec06
Igor <silveira.igor@outlook.com>
2023-01-11 23:40:58 -0300

    fix: fix problems in pets routes

--- src/routes/pets.routes.ts
+++ src/routes/pets.routes.ts
@@ -1,12 +1,24 @@
 import { Router } from "express";
-import { listPetsController, createPetController, listPetByIdController } from "../controllers";
-import { verifyAuth, verifyPetIdParameter, verifyRequestPerSchema } from "../middleware";
+import {
+  listPetsController,
+  createPetController,
+  listPetByIdController,
+} from "../controllers";
+import {
+  verifyAuth,
+  verifyPetIdParameter,
+  verifyRequestPerSchema,
+} from "../middleware";
 import { petRequestSchema } from "../schemas";
 
 export const petsRoutes = Router();
 
-petsRoutes.get("/pets", listPetsController)
-petsRoutes.get("/pets/:id", listPetByIdController)
+petsRoutes.get("/pets", listPetsController);
+petsRoutes.get("/pets/:id", verifyPetIdParameter, listPetByIdController);
 
-petsRoutes.post("/pets",verifyRequestPerSchema(petRequestSchema), verifyAuth, createPetController) 
-petsRoutes.post(`/pets/:id`, verifyPetIdParameter, verifyRequestPerSchema(petRequestSchema), verifyAuth, createPetController) 
+petsRoutes.post(
+  "/pets",
+  verifyRequestPerSchema(petRequestSchema),
+  verifyAuth,
+  createPetController
+);
