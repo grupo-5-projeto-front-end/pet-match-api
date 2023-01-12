cf99294d67999d6d1bf4dff250918a16b25735e4
douglasB834 <douglas_borges09@hotmail.com>
2023-01-12 19:19:28 -0300

    chore: add testes deploy

--- src/routes/pets.routes.ts
+++ src/routes/pets.routes.ts
@@ -9,10 +9,10 @@ export const petsRoutes = Router();
 
 petsRoutes.get("/pets", listPetsController);
 petsRoutes.get("/pets/:id", verifyPetIdParameter, listPetByIdController);
-petsRoutes.get("/pets/user/:id",verifyUserIdParameter, verifyAuth, listPetsByUserController);
 
 
 petsRoutes.post("/pets",verifyRequestPerSchema(petRequestSchema), verifyAuth, createPetController) 
 petsRoutes.patch(`/pets/:id`, verifyPetIdParameter, verifyRequestPerSchema(petUpdateRequestSchema), verifyAuth, patchUPetController) 
 petsRoutes.delete(`/pets/:id`, verifyPetIdParameter, verifyAuth, softDeletePetController) 
+petsRoutes.get("/pets/user/:id",verifyUserIdParameter, verifyAuth, listPetsByUserController);
 
