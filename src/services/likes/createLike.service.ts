b7a9b7f2672dbf396f0246d0e7fede0208736e36
Igor <silveira.igor@outlook.com>
2023-01-13 20:16:46 -0300

    fix: fix a problem in create like request

--- src/services/likes/createLike.service.ts
+++ src/services/likes/createLike.service.ts
@@ -2,6 +2,7 @@ import { AppDataSource } from "../../data-source";
 import { Likes } from "../../entities/likesEntity";
 import { Pets } from "../../entities/petsEntity";
 import { Users } from "../../entities/usersEntity";
+import AppError from "../../errors/AppError";
 import { petResponseSchema } from "../../schemas";
 
 export const createLikeService = async (
@@ -14,6 +15,18 @@ export const createLikeService = async (
 
   const user = await usersRepo.findOneBy({ id: userId });
   const pet = await petsRepo.findOneBy({ id: petId });
+
+  const like = await likesRepo
+    .createQueryBuilder("l")
+    .where("l.userId = :userid AND l.petId = :petid", {
+      userid: userId,
+      petid: petId,
+    })
+    .getOne();
+
+  if (like) {
+    throw new AppError("You can't like this pet again", 409);
+  }
 
   const newLike = likesRepo.create({
     pet: pet,
