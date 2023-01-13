d6115291f088691ccad6aee6a0c030dd6cc5ca78
Vinicius <v.nascimento23@yahoo.com.br>
2023-01-12 21:49:58 -0300

    refactor: changed some stuff for testing

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,11 @@ import {app} from "./app";
 import { AppDataSource } from "./data-source";
 import "dotenv/config";
 
-const PORT = 3000 || process.env.PORT;
+AppDataSource.initialize()
+.then(() => {
+    console.log("Database running");
+    const PORT = process.env.PORT;
 
-AppDataSource.initialize().catch(err => console.error("Error during Data Source initialization", err));
-
-app.listen(3000, () => console.log(`Server running on port ${PORT}`));
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+})
+.catch(err => console.error("Error during Data Source initialization", err));
